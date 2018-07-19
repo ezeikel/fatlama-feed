a70c033af1e035cd3c8f473493562833ce49a86f
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-07-19 22:42:47 +0100

    WIP Prev/Next.

--- fatlama-feed/src/components/Feed/Feed.js
+++ fatlama-feed/src/components/Feed/Feed.js
@@ -11,33 +11,47 @@ class Feed extends Component {
 
   componentDidMount() {
     console.log('componentDidMount');
+    this.fetchData();
+  }
+
+  fetchData = (page = this.state.page) => {
     const url = 'http://localhost:8080/transactions';
-    const current = axios.get(`${url}/${this.state.page}`);
-    const next = axios.get(`${url}/${this.state.page + 1}`);
+    const current = axios.get(`${url}/${page}`);
+    const next = axios.get(`${url}/${page + 1}`);
 
     Promise.all([current, next])
       .then(([current, next]) => {
-        this.setState((prevState) => {
-          return {
-            transactions: current.data,
-            previous: prevState.page !== 1,
-            next: next.data.length > 0
-          }
-        })
+        console.log('Setting state!');
+        this.setState({
+          page: page,
+          transactions: current.data,
+          previous: page !== 1,
+          next: next.data.length > 0
+        });
+      })
+      .catch(e => {
+        if (e.request.status === 500) {
+          this.setState({
+            page: page,
+            transactions: this.state.transactions,
+            previous: page !==1,
+            next: false
+          })
+        }
+        debugger;
+        console.log('Erroring out!');
+        console.log({error: e})
       });
   }
 
   handleNextClick = () => {
     console.log('Next..');
-    this.setState((prevState) => {
-      return {
-        page: prevState.page + 1
-      }
-    });
+    this.fetchData(this.state.page + 1);
   }
 
   handlePreviousClick = () => {
     console.log('Previous..');
+    this.fetchData(this.state.page - 1);
   }
 
   renderTransactions = () => (
@@ -50,8 +64,10 @@ class Feed extends Component {
   );
 
   render() {
+    console.log('render()');
     return (
       <React.Fragment>
+        <h1>Page: {this.state.page}</h1>
         <ul>
           {this.renderTransactions()}
         </ul>
