79afa8bec58e07cde563b3a5b947ea25522f98a5
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-07-21 16:42:06 +0100

    Loading transactions data upfront and storing pages in state.

--- fatlama-feed/src/components/Feed/Feed.js
+++ fatlama-feed/src/components/Feed/Feed.js
@@ -4,75 +4,81 @@ import axios from 'axios';
 class Feed extends Component {
   state = {
     page: 1,
-    previous: false,
-    next: false,
-    transactions: [],
+    totalPages: 0,
+    transactions: {}
   };
 
   componentDidMount() {
-    console.log('componentDidMount');
     this.fetchData();
   }
 
-  fetchData = (page = this.state.page) => {
+  fetchData = async () => {
+    let morePages = true;
+    let index = 0;
     const url = 'http://localhost:8080/transactions';
-    const current = axios.get(`${url}/${page}`);
-    const next = axios.get(`${url}/${page + 1}`);
 
-    Promise.all([current, next])
-      .then(([current, next]) => {
-        console.log('Setting state!');
+    while(morePages) {
+      let response;
+      try {
+        response = await axios.get(`${url}/${index}`);
+      } catch(error) {
+        morePages = false;
         this.setState({
-          page: page,
-          transactions: current.data,
-          previous: page !== 1,
-          next: next.data.length > 0
+          totalPages: index
         });
-      })
-      .catch(e => {
-        if (e.request.status === 500) {
-          this.setState({
-            page: page,
-            transactions: this.state.transactions,
-            previous: page !==1,
-            next: false
-          })
+        return;
+      }
+
+      this.setState({
+        transactions: {
+          ...this.state.transactions,
+          [index + 1]: response.data
         }
-        debugger;
-        console.log('Erroring out!');
-        console.log({error: e})
       });
+
+      index++;
+    }
   }
 
   handleNextClick = () => {
-    console.log('Next..');
-    this.fetchData(this.state.page + 1);
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+      }
+    });
   }
 
   handlePreviousClick = () => {
-    console.log('Previous..');
-    this.fetchData(this.state.page - 1);
+    this.setState(prevState => {
+      return {
+        page: prevState.page - 1,
+      }
+    });
   }
 
-  renderTransactions = () => (
-    this.state.transactions.map(transaction => (
-      <li key={transaction.id}>
-        <span>id: {transaction.id}</span>
-        <span>status: {transaction.status}</span>
-      </li>
-    ))
-  );
+  renderTransactions = () => {
+    if (this.state.transactions[this.state.page]) {
+      return this.state.transactions[this.state.page].map(transaction => (
+        <li key={transaction.id}>
+          <span>id: {transaction.id}</span>
+          <span>status: {transaction.status}</span>
+        </li>
+      ))
+    } else {
+      return <li>No transactions</li>
+    }
+  }
 
   render() {
-    console.log('render()');
     return (
       <React.Fragment>
         <h1>Page: {this.state.page}</h1>
+        <h4>Total Pages: {this.state.totalPages}</h4>
         <ul>
           {this.renderTransactions()}
         </ul>
-        { this.state.previous ? <span onClick={this.handlePreviousClick}>Previous</span> : null }
-        { this.state.next ? <span onClick={this.handleNextClick}>Next</span> : null }        
+        { this.state.page !== 1 ? <button onClick={this.handlePreviousClick}>Previous</button> : null }
+        { this.state.page !== this.state.totalPages ? <button onClick={this.handleNextClick}>Next</button> : null }        
       </React.Fragment>
     )
   }
