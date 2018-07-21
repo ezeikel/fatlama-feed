39687b5a60d72c08c305bfefd6980221ac66d477
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-07-21 19:03:19 +0100

    Select on transaction page is prepopulated.

--- fatlama-feed/src/components/Transaction/Transaction.js
+++ fatlama-feed/src/components/Transaction/Transaction.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const STATUS_CODES = [
+  'ESCROW',
+  'FL_APPROVED',
+  'PAID',
+  'PRE_AUTHORIZED',
+  'PRE_AUTHORIZED_CANCELLED',
+  'QUARANTINED'
+];
+
 class Transaction extends Component {
   state = {
-    data: {}
+    transaction: {},
+    updatedStatus: ''
   }
 
   componentDidMount() {
@@ -13,16 +23,42 @@ class Transaction extends Component {
     axios.get(`${url}/transaction/${params.id}`)
       .then(response => {
         this.setState({
-          data: response.data
-        })
-      })
+          transaction: response.data
+        });
+      });
+  }
+
+  createSelectItems = () => {
+    let items = [];
+    const status = this.state.transaction.status;
+
+    STATUS_CODES.forEach(code => {
+      const selected = status === code;
+      items.push(
+        <option selected={selected} key={code} value={code}>{code}</option>
+      )
+    });
+    return items;
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted');
   }
 
   render() {
     return (
       <React.Fragment>
-        <h1>{this.state.data.id}</h1>
-        <h3>{this.state.data.status}</h3>
+        <h1>{this.state.transaction.id}</h1>
+        <h3>{this.state.transaction.status}</h3>
+        <h2>Change Status</h2>
+        <form onSubmit={this.handleSubmit}>
+          <select>
+            <option value="">Select a transaction status</option>
+            {this.createSelectItems()}
+          </select>
+          <input type="submit" value="Submit" />
+        </form>
       </React.Fragment>
     );
   }
